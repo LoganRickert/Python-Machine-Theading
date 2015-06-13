fdfaf5cc2a4c2630a5586094eae8912d537f819a
Logan Rickert <logan@rickertcorp.com>
2015-06-13 00:23:23 -0400

    Start of functional

--- client.py
+++ client.py
@@ -1,58 +1,113 @@
 # Echo client program
 import socket
 import time
-import pickle
+import json
 import random
-
+import sys
+import os
 
 class Client(object):
 
 	def __init__(self):
 		self.__host = ''
-		self.__port = 50000
-		self.__set_port()
+		self.__port = 50001
 
 		self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.__socket.bind((self.__host, self.__port))
 		self.__socket.listen(5)
 
-		self.__queue = []
-		self.__count = 0
-		self.__hit = 0
+		self.__commands = {
+			'stop': self.__stop,
+			'process': self.__process,
+			'print': self.__print,
+			'restart': self.__restart
+		}
 
-	def __set_port(self):
-		with open('port', 'r') as f:
-			self.__port = int(f.read())
+		self.__keepRunning = True
 
-	def processData(data):
+	def start(self):
+		while self.__keepRunning:
+			conn, addr = self.__socket.accept()
+			data = conn.recv(2048)
+			temp_data = data
+
+			while temp_data != "" and len(temp_data) > 2047:
+				temp_data = conn.recv(2048)
+				data += temp_data
+
+			arguments = data.split()
+
+			print 'Recieved command: ' + data + '', arguments[1:]
+
+			if data in self.__commands:
+				self.__commands[arguments[0]](arguments[1:])
+
+	def __stop(self, arguments = []):
+		self.__keepRunning = False
+
+	def __restart(self, arguments = []):
+		print 'restarting...'
+		self.__stop()
+		self.stop()
+		print 'starting...'
+		os.execv(sys.executable, [sys.executable] + sys.argv)
+
+	def __print(self, arguments = []):
+		print 'hello!'
+
+	def stop(self):
+		print 'closing server...'
+		self.__socket.close()
+
+	def __processData(self, data):
 		for i in range(0, len(data[1])):
 			data[1][i] += 5
 		return data
 
-what = [-1]
+	def __process(self, arguments = []):
+		what = [-1]
 
-while what != []:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((HOST, PORT))
-		if len(what) == 2:
-			what = processData(what)
-			time.sleep(1)
-			print 'Sending ', what
-			s.sendall(pickle.dumps(what))
-		else:
-			s.sendall(pickle.dumps([]))
-		data = s.recv(1024)
-		s.close()
-		what = pickle.loads(data)
-		print 'Received ', what
-	except:
-		print 'Error while connecting'
-		what = []
+		time.sleep(5)
+
+		host = ''
+		port = 50000
+
+		# with open('port', 'r') as f:
+		# 	port = int(f.read())
+
+		while what != []:
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				s.connect((host, port))
+				if len(what) == 2:
+					what = self.__processData(what)
+					s.sendall(json.dumps(what))
+				else:
+					s.sendall("{}")
+
+				temp_data = s.recv(1024)
+				data = temp_data
+
+				while temp_data:
+					temp_data = s.recv(1024)
+					data += temp_data
+
+				s.close()
+				what = json.loads(data)
+				# print 'Received ', what
+			except:
+				print 'Error while connecting'
+				print sys.exc_info()
+				what = []
+
+		print 'done.'
 
 def main():
 	client = Client()
 	client.start()
+	client.stop()
 
 if __name__ == "__main__":
 	main()
--- server.py
+++ server.py
@@ -1,22 +1,28 @@
 # Echo server program
 import socket
-import pickle
+import sys
+import json
 from thread import *
+import time
 
 class Server(object):
 
 	def __init__(self):
 		self.__host = ''
 		self.__port = 50000
-		self.__set_port()
+		# self.__set_port()
 
 		self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.__socket.bind((self.__host, self.__port))
 		self.__socket.listen(5)
 
 		self.__queue = []
 		self.__count = 0
 		self.__hit = 0
+
+		self.__time = 0
+		self.__start = False
 
 	def __set_port(self):
 		with open('port', 'r') as f:
@@ -26,25 +32,51 @@ class Server(object):
 			f.write(str(self.__port))
 
 	def __clientthread(self, conn):
-		data = conn.recv(1024)
+		data = conn.recv(2048)
+		temp_data = data
+
+		while temp_data != "" and len(temp_data) > 2047:
+			temp_data = conn.recv(2048)
+			data += temp_data
+
 		if data:
-			addQueue = pickle.loads(data)
+			addQueue = json.loads(data)
 			if len(addQueue) > 0:
-				print 'recv the data ', addQueue
+				# print 'recv the data ', addQueue
 				self.__updatedQueue.append(addQueue)
 		if self.__count < len(self.__queue):
-			sendList = [self.__hit, self.__queue[self.__count:self.__count+2]]
-			self.__count += 2
+			sendList = [self.__hit, self.__queue[self.__count:self.__count+1000]]
+			self.__count += 1000
+			if self.__count % 50000 == 0:
+				print self.__count
 			self.__hit += 1
-			conn.sendall(pickle.dumps(sendList))
-			print 'Sent ', sendList
+			conn.sendall(json.dumps(sendList))
+			# print 'Sent ', sendList
 		else:
-			conn.sendall(pickle.dumps([]))
-			print 'Sent []'
+			conn.sendall(json.dumps([]))
+			# print 'Sent []'
 		conn.close()
 
 	def start(self):
-		self.__queue = range(1, 101)
+		client_list = [['', 50001]]
+
+		for client in client_list:
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.connect((client[0], client[1]))
+				print 'Please enter command: '
+				command = raw_input()
+				if command == "process":
+					s.sendall(command)
+					self.__process()
+				else:
+					s.sendall(command)
+			except:
+				print 'Error while connecting'
+				print sys.exc_info()
+
+	def __process(self):
+		self.__queue = range(1, 10000)
 		self.__count = 0
 		self.__hit = 0
 
@@ -52,15 +84,28 @@ class Server(object):
 
 		while self.__count < len(self.__queue):
 			conn, addr = self.__socket.accept()
-			print 'Connected by', addr
+			# print 'Connected by', addr
+			if not self.__start:
+				self.__time = time.time()
+				self.__start = True
 			start_new_thread(self.__clientthread, (conn,))
 
+		self.sort()
+
+
+	def sort(self):
+		time.sleep(2)
 		self.__updatedQueue.sort(key=lambda x: x[0])
 		print self.__updatedQueue
+		print time.time() - self.__time - 2
+
+	def stop(self):
+		self.__socket.close()
 
 def main():
 	server = Server()
 	server.start()
+	server.stop()
 
 if __name__ == "__main__":
 	main()
